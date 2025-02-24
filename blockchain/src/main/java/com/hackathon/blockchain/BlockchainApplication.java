a523913d7b9606a2b791562303730a3e70662bb6
David Cediel <esdago18@gmail.com>
2025-02-24 07:13:51 +0100

    Move application runner bean to a separate class

--- blockchain/src/main/java/com/hackathon/blockchain/BlockchainApplication.java
+++ blockchain/src/main/java/com/hackathon/blockchain/BlockchainApplication.java
@@ -1,20 +1,9 @@
 package com.hackathon.blockchain;
 
-import com.hackathon.blockchain.dto.request.Contract;
-import com.hackathon.blockchain.repository.SmartContractRepository;
-import com.hackathon.blockchain.service.BlockchainService;
-import com.hackathon.blockchain.service.contract.ContractService;
-import com.hackathon.blockchain.service.transaction.FeeService;
-import com.hackathon.blockchain.service.wallet.WalletService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.Transactional;
-
-import static com.hackathon.blockchain.utils.AssetConstants.INITIAL_LIQUIDITY_POOL;
 
 @SpringBootApplication(scanBasePackages = "com.hackathon.blockchain")
 @EnableScheduling
@@ -25,40 +14,5 @@ public class BlockchainApplication {
         SpringApplication.run(BlockchainApplication.class, args);
     }
 
-    @Bean
-    @Transactional
-    public ApplicationRunner runner(WalletService walletService,
-                                    BlockchainService blockchainService,
-                                    ContractService contractService,
-                                    SmartContractRepository smartContractRepository,
-                                    FeeService feeService) {
 
-        return appArgs -> {
-            log.info("Initializing liquidity pools");
-
-            try {
-                walletService.initializeLiquidityPools(INITIAL_LIQUIDITY_POOL);
-            } catch (Exception e) {
-                log.error("Error creating the key for BTC wallet", e);
-            }
-            log.info(feeService.createFeeWallet());
-            log.info("Liquidity pool initialized");
-            blockchainService.createGenesisBlock();
-
-            walletService.getWalletByAddress("LP-BTC").ifPresent(wallet -> {
-                Boolean contractAlreadyExists = smartContractRepository.existsByIssuerWalletId(wallet.getId());
-
-                if (Boolean.TRUE.equals(contractAlreadyExists)) {
-                    return;
-                }
-
-                contractService.createContract(new Contract("anti-whale",
-                        "#amount > 10 and #txType == 'BUY'",
-                        "CANCEL_TRANSACTION", 0.0, wallet.getId()));
-                log.info("Contract for liquidity pool BTC created!");
-            });
-
-        };
-
-    }
 }
