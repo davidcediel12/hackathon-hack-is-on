cd47166c4b2e76a1b9bf3bf3ef156b84c609ceb5
David Cediel <esdago18@gmail.com>
2025-02-24 07:14:21 +0100

    Create test to check the controller layer

---
+++ blockchain/src/test/java/com/hackathon/blockchain/controller/AuthenticationControllerTest.java
@@ -0,0 +1,47 @@
+package com.hackathon.blockchain.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hackathon.blockchain.dto.request.UserRegistration;
+import com.hackathon.blockchain.service.authentication.AuthService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = AuthenticationController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class AuthenticationControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockitoBean
+    AuthService authService;
+
+    @Test
+    void shouldCreateUser() throws Exception {
+
+        UserRegistration userRegistration = new UserRegistration("foo@f.com", "bar", "baz");
+
+        doNothing().when(authService).registerUser(any(), any(), any());
+
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRegistration)))
+                .andExpect(status().isOk());
+    }
+
+}
