e53edd37073bb4f01d1ca252ee7343084921090c
David Cediel <esdago18@gmail.com>
2025-02-24 09:46:54 +0100

    Create an interface for MarketDataService

--- blockchain/src/main/java/com/hackathon/blockchain/service/MarketDataService.java
+++ blockchain/src/main/java/com/hackathon/blockchain/service/MarketDataService.java
@@ -1,44 +1,11 @@
 package com.hackathon.blockchain.service;
 
-import com.hackathon.blockchain.exception.ApiException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-
 import java.util.Map;
 
-import static com.hackathon.blockchain.utils.MessageConstants.ASSET_NOT_FOUND;
+public interface MarketDataService {
+    String getMarketPrice(String symbol);
 
-@Service
-@RequiredArgsConstructor
-public class MarketDataService {
+    double fetchLivePriceForAsset(String symbol);
 
-    private final RestClient restClient;
-
-    private static final String API_URL = "https://faas-lon1-917a94a7.doserverless.co/api/v1/web/fn-3d8ede30-848f-4a7a-acc2-22ba0cd9a382/default/fake-market-prices";
-
-
-    public String getMarketPrice(String symbol){
-        return "Current price of " + symbol + ": $" + fetchLivePriceForAsset(symbol);
-    }
-    public double fetchLivePriceForAsset(String symbol) {
-        Double assetPrice = fetchLiveMarketPrices().get(symbol);
-
-        if(assetPrice == null) {
-            throw new ApiException(ASSET_NOT_FOUND + symbol, HttpStatus.BAD_REQUEST);
-        }
-
-        return assetPrice;
-    }
-
-    public Map<String, Double> fetchLiveMarketPrices() {
-
-        return restClient.get()
-                .uri(API_URL)
-                .retrieve()
-                .body(new ParameterizedTypeReference<Map<String, Double>>() {
-                });
-    }
+    Map<String, Double> fetchLiveMarketPrices();
 }
---
+++ blockchain/src/main/java/com/hackathon/blockchain/service/MarketDataServiceImpl.java
@@ -0,0 +1,48 @@
+package com.hackathon.blockchain.service;
+
+import com.hackathon.blockchain.exception.ApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+import static com.hackathon.blockchain.utils.MessageConstants.ASSET_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class MarketDataServiceImpl implements MarketDataService {
+
+    private final RestClient restClient;
+
+    private static final String API_URL = "https://faas-lon1-917a94a7.doserverless.co/api/v1/web/fn-3d8ede30-848f-4a7a-acc2-22ba0cd9a382/default/fake-market-prices";
+
+    @Override
+    public String getMarketPrice(String symbol){
+        return "Current price of " + symbol + ": $" + fetchLivePriceForAsset(symbol);
+    }
+
+    @Override
+    public double fetchLivePriceForAsset(String symbol) {
+        Double assetPrice = fetchLiveMarketPrices().get(symbol);
+
+        if(assetPrice == null) {
+            throw new ApiException(ASSET_NOT_FOUND + symbol, HttpStatus.BAD_REQUEST);
+        }
+
+        return assetPrice;
+    }
+
+
+    @Override
+    public Map<String, Double> fetchLiveMarketPrices() {
+
+        return restClient.get()
+                .uri(API_URL)
+                .retrieve()
+                .body(new ParameterizedTypeReference<Map<String, Double>>() {
+                });
+    }
+}
